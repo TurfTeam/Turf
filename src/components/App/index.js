da0169c8d6ddec0d67e0a6889fa2f2244f91073f
Hunter Jarrell <hjarrell555@gmail.com>
2018-11-26 11:07:01 -0500

    Add routing to project

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+
+import * as ROUTES from '../../constants/routes';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+    </div>
+  </Router>
 );
 
 export default App;

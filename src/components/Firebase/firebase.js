215634eafde3d9944c5fa3080ef187f7be7b1d80
Hunter Jarrell <hjarrell555@gmail.com>
2018-11-26 11:27:44 -0500

    Add methods for firebase auth

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyA6WokB6mgmPp-j9EjFf5hgPuTyNcWS5Fg",
@@ -12,7 +13,22 @@ const config = {
 class Firebase {
     constructor() {
         app.initializeApp(config);
+        
+        this.auth = app.auth();
     }
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;

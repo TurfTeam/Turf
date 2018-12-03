31dc8a9de3be238b43284af2cb5409e66f953b16
Sylvia Lujo <thenightowl@Sylvias-MacBook-Pro.local>
2018-12-03 00:38:58 -0500

    Update: landing page

--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -12,7 +12,7 @@ const withAuthorization = condition => Component => {
       this.listener = this.props.firebase.auth.onAuthStateChanged(
         authUser => {
           if (!condition(authUser)) {
-            this.props.history.push(ROUTES.SIGN_IN);
+            this.props.history.push(ROUTES.LANDING);
           }
         },
       );
